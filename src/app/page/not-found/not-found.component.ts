441fc3520bb7395931b8e881c18cf7b0f51ec7a4
apletenev <apletenev@htc-cs.ru>
2020-12-16 16:58:00 +0400

    Правки адаптива, добавление контента на 404 страницу

--- src/app/page/not-found/not-found.component.ts
+++ src/app/page/not-found/not-found.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements AfterViewInit {
 
+  @ViewChild('notFound') notFound?: ElementRef;
+  private fullHeightWindow = 0;
+  private headerHeight = 0;
+  private footerHeight = 0;
+  heightContainer = 0;
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    if (this.notFound) {
+      const height = document.querySelector('.header') as HTMLElement;
+      const footer = document.querySelector('.footer') as HTMLElement;
+
+      this.fullHeightWindow = window.innerHeight;
+      this.headerHeight = !!height ? height.offsetHeight : 0;
+      this.footerHeight = !!footer ? footer.offsetHeight + 72 : 0;
+
+      this.notFound.nativeElement.style.height = this.fullHeightWindow - (this.headerHeight + this.footerHeight) + 'px';
+      console.log(this.headerHeight, this.footerHeight);
+    }
+  }
 }
